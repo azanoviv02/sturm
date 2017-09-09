858f68c2475a523d9811bc148dcb223b49fba4b1
Ivan Azanpv <azanoviv02@gmail.com>
2017-09-09 23:16:05 +0300

    Added db settings

--- sturm/settings/base.py
+++ sturm/settings/base.py
@@ -96,22 +96,22 @@ WSGI_APPLICATION = 'sturm.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'HOST': 'localhost',
-#         'NAME': 'hello',
-#         'USER': 'hello',
-#         'PASSWORD': 'changeme',
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
 #     }
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'HOST': 'localhost',
+        'NAME': 'hello',
+        'USER': 'hello',
+        'PASSWORD': '456456',
+    }
+}
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
